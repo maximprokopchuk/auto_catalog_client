91048f53ab1954a8239558522255befaf9ebbd4c
Maxim Prokopchuk <proroker100@gmail.com>
2024-04-05 15:33:54 +0700

    fix(client): refactor and fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import Storehouses from "./features/storehouses/Storehouses";
+import Storehouses from "./components/Storehouses/Storehouses";
 import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes//lara-dark-amber/theme.css";
 import "primeicons/primeicons.css";
 import classes from "./classes.module.css";
-import StorehouseItems from "./features/storehouse_items/StorehouseItems";
+import StorehouseItems from "./components/StorehouseItems/StorehouseItems";
+import { Panel } from "primereact/panel";
+import CarModels from "./components/CarModels/CarModels";
+import { ScrollPanel } from "primereact/scrollpanel";
 
 const App = () => {
   return (
     <PrimeReactProvider>
       <div className={classes.root}>
         <div className={classes.left}>
+          <CarModels />
           <div className={classes.storehouses}>
             <Storehouses />
           </div>
-          <div>
-            <StorehouseItems />
-          </div>
+          <StorehouseItems />
         </div>
-        <div>Component Settings</div>
+        <div className={classes.right}>
+          <Panel header="Settings">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam id nam animi mollitia odio? A nesciunt, doloremque blanditiis sequi aspernatur maiores minima unde numquam iusto, harum ex laboriosam optio amet.</Panel>
+        </div>
       </div>
     </PrimeReactProvider>
   );
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit";
 import { combineSlices, configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import { storehousesSlice } from "../features/storehouses/storehouseSlice";
-import { storehousComponentsSlice } from "../features/storehouse_items/storehouseItemsSlice";
-import { uiSlice } from "./uiSlice";
+import { storehousesSlice } from "./slices/storehouseSlice";
+import { storehousComponentsSlice } from "./slices/storehouseItemsSlice";
+import { uiSlice } from "./slices/uiSlice";
 
 const rootReducer = combineSlices(
   storehousesSlice,
